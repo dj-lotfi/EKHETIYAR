50e2fb097b27aed79499555765cf41b2de4f8aa0
dj-lotfi <124455113+dj-lotfi@users.noreply.github.com>
2023-04-01 23:17:40 +0100

    now all banks show correclty

--- app/Views/accueil/index.php
+++ app/Views/accueil/index.php
@@ -87,7 +87,13 @@
                                         echo '<tr>';
                                         echo '<td>' . iconv('ISO-8859-1', 'UTF-8', $prpr[$i]->nom) . '</td>';
                                         echo '<td>' . iconv('ISO-8859-1', 'UTF-8', $prpr[$i]->categorie) . '</td>';
-                                        echo '<td>' . iconv('ISO-8859-1', 'UTF-8', $prpr[$i]->prix) . '</td>';
+                                        if ($prpr[$i]->prix==null) {
+                                            echo '<td>' .'?'. '</td>';
+                                        } else {
+                                            echo '<td>' . iconv('ISO-8859-1', 'UTF-8', $prpr[$i]->prix) . '</td>';
+                                        }
+                                        
+                                        
                                         echo '</tr>';
                                     }
                                     ?>
@@ -118,43 +124,35 @@
                         Site:
                         <?php echo iconv('ISO-8859-1', 'UTF-8', $bank->site_banque) ?>
                     </p>
-                    <button id="<?php echo $buttonId; ?>" class="prestations-button" >Prestations</button>
-                    <!-- The Modal -->
-                    <div id="<?php echo $modalId; ?>" class="modal">
-                        <!-- Modal content -->
-                        <div class="modal-content">
-                            <span class="close">&times;</span>
-                            <h2>Prestations</h2>
-                            <?php displayPrestations($bankId); ?>
-                            
+                    <div>
+                        <button id="<?php echo $buttonId; ?>" class="prestations-button" >Prestations</button>
+                        <!-- The Modal -->
+                        <div id="<?php echo $modalId; ?>" class="modal">
+                            <!-- Modal content -->
+                            <div class="modal-content">
+                                <span class="close" >&times;</span>
+                                <h2>Prestations</h2>
+                                <?php displayPrestations($bankId); ?>
+                                
+                            </div>
                         </div>
                     </div>
+                    
                     <script>
-                        // Get the modal
-                        var modal = document.getElementById("<?php echo $modalId; ?>");
 
-                        // Get the button that opens the modal
-                        var btn = document.getElementById("<?php echo $buttonId; ?>");
+                        document.getElementById("<?php echo $buttonId; ?>").onclick = function() {myFunction<?php echo $bankId; ?>()};
+                        document.getElementById("<?php echo $modalId; ?>").getElementsByClassName("close")[0].onclick = function() {
 
-                        // Get the <span> element that closes the modal
-                        var span = modal.getElementsByClassName("close")[0];
+                            document.getElementById("<?php echo $modalId; ?>").style.display = "none";
+                        };
+                        document.getElementById("<?php echo $buttonId; ?>").onclick = function() {
+                            
+                            document.getElementById("<?php echo $modalId; ?>").style.display = "block";
+                        };
 
-                        // When the user clicks on the button, open the modal
-                        btn.onclick = function () {
-                            modal.style.display = "block";
-                        }
+                        
 
-                        // When the user clicks on <span> (x), close the modal
-                        span.onclick = function () {
-                            modal.style.display = "none";
-                        }
 
-                        // When the user clicks anywhere outside of the modal
-                        window.onclick = function (event) {
-                            if (event.target == modal) {
-                                modal.style.display = "none";
-                            }
-                        }
                     </script>
                 </div>
 
