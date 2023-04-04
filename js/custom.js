3a6897409e8cdc469a32e917b7550fcbdbf70e03
BespectacledBeaver <124437379+BespectacledBeaver@users.noreply.github.com>
2023-04-04 19:59:47 +0000

    Changed bank cards look, updated CSS and JS

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,54 @@
-const carouselContainer = document.querySelector('.carousel-container');
+var body = document.body;
+var bankCards = document.querySelectorAll('.bank-card');
+
+bankCards.forEach(function (bankCard) {
+
+  var viewButton = bankCard.querySelector('.view-button');
+  var prestationsButton = bankCard.querySelector('.prestations-button__handler');
+  var modal = bankCard.querySelector('.modal');
+  var closeButton = bankCard.querySelector('.close');
+
+  viewButton.onclick = function () {
+
+    var moreInfo = bankCard.querySelector('.more-info');
+
+    if (moreInfo.style.display === 'none' || moreInfo.style.display === "") {
+      moreInfo.style.display = 'block';
+      moreInfo.style.animation = 'fade-in 1s';
+      viewButton.innerHTML = 'Voir Moins';
+    } else {
+      moreInfo.style.animation = 'fade-out 1s';
+      setTimeout(function () { moreInfo.style.display = 'none'; viewButton.innerHTML = 'Voir Plus'; }, 800);
+    }
+
+  };
+
+  prestationsButton.onclick = function () {
+
+    modal.style.display = "block";
+    body.style.overflowY = 'hidden'; /* Hide scrollbar and remove scrolling functionality */
+    
+  };
+
+  closeButton.onclick = function () {
+
+    modal.style.display = "none";
+    body.style.overflowY = 'overlay'; /* Show scrollbar */
+
+  };
+
+  modal.onclick = function () {
+
+    if (click.target == modal) {
+      modal.style.display = "none";
+      body.style.overflowY = 'overlay'; /* Show scrollbar */
+    }
+
+  };
+
+});
+
+/*const carouselContainer = document.querySelector('.carousel-container');
 const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
 const carouselItems = document.querySelectorAll('.carousel-item');
@@ -50,4 +100,4 @@ function updateDots() {
 
 prevButton.addEventListener('click', handlePrevButtonClick);
 
-nextButton.addEventListener('click', handleNextButtonClick);
+nextButton.addEventListener('click', handleNextButtonClick);*/
--- js/prestation.js
+++ js/prestation.js
@@ -7,6 +7,8 @@ document.getElementById("<?php echo $buttonId; ?>").onclick = function() {
     document.getElementById("<?php echo $modalId; ?>").style.display = "block";
 };*/
 
+
+/*
 var bankCards = document.querySelectorAll('.rectangle');
 var body = document.body;
 
@@ -17,18 +19,18 @@ bankCards.forEach(bankCard => {
 
     prestationsButton.addEventListener('click', () => {
         modal.style.display = "block";
-        body.style.overflowY = 'hidden'; /* Hide scrollbar and remove scrolling functionality */
+        body.style.overflowY = 'hidden'; /* Hide scrollbar and remove scrolling functionality
     });
 
     closeButton.addEventListener('click', () => {
         modal.style.display = "none";
-        body.style.overflowY = 'overlay'; /* Show scrollbar */
+        body.style.overflowY = 'overlay'; /* Show scrollbar
     });
 
     modal.addEventListener('click', (click) => {
         if (click.target == modal ) {
         modal.style.display = "none";
-        body.style.overflowY = 'overlay'; /* Show scrollbar */
+        body.style.overflowY = 'overlay'; /* Show scrollbar
         }
     });
-});
+});*/
