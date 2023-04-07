82576859ed9fb943cc8d15ed4a1c5446fd21e17f
Folokee <lr_menassel@esi.dz>
2023-04-07 05:21:12 +0100

    started work on comparatif

--- app/Views/comparatif/index.php
+++ app/Views/comparatif/index.php
@@ -2,75 +2,110 @@
 
 <?php $this->start('head'); ?>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/comparatif.css">
+</head>
+
 </html>
 <?php $this->end(); ?>
 
 <?php $this->start('body'); ?>
 <!DOCTYPE html>
 <html>
-    <body class="main-layout">
-        <header>
-            <h3 class="logo">Logo</h3>
-            <input type="checkbox" id="nav-toggle" class="nav-toggle">
-            <nav class="navbar">
-                <ul>
-                <li><a href="<?=PROOT?>/accueil">Accueil</a></li>
-                <li><a href="<?=PROOT?>/comparatif">Comparatif</a></li>
-                <li><a href="<?=PROOT?>/qui_sommes_nous">Qui Sommes-Nous</a></li>
-                </ul>
-            </nav>
-            <label for="nav-toggle" class="nav-toggle-label">
-                <span></span>
-            </label>
-        </header>
-        <section>
-            <div class="carousel">
-    
-                <button class="carousel__button carousel__button--left">
-                    <img src="<?=PROOT?>/img/left.svg" alt="">
-                </button>
-    
-                <div class="coursel__track-container">
-                    <ul class="carousel__track">
-                        <li class="carousel__slide current-slide">
-                            <img class="carousel__image" src="<?=PROOT?>/img/img.jpg" alt="">
-                        </li>
-                        <li class="carousel__slide">
-                            <img class="carousel__image" src="<?=PROOT?>/img/img1.webp" alt="">
-                        </li>
-                        <li class="carousel__slide">
-                            <img class="carousel__image" src="<?=PROOT?>/img/img2.jpg" alt="">
-                        </li>
-                    </ul>
-                </div>
-    
-                <button class="carousel__button carousel__button--right">
-                    <img src="<?=PROOT?>/img/right.svg" alt="">
-                </button>
-    
-                <div class="carousel__nav">
-                    <button class="carousel__indicator current-slide"></button>
-                    <button class="carousel__indicator"></button>
-                    <button class="carousel__indicator"></button>
-                </div>
-    
-            </div>
-        </section>
-        <main class="content-layout">
-        </main>
-        <footer>
+
+<body class="main-layout">
+    <header>
+        <h3 class="logo">Logo</h3>
+        <input type="checkbox" id="nav-toggle" class="nav-toggle">
+        <nav class="navbar">
             <ul>
-                <li><a href="<?=PROOT?>/accueil">Accueil</a></li>
-                <li><a href="<?=PROOT?>/comparatif">Comparatif</a></li>
-                <li><a href="<?=PROOT?>/qui_sommes_nous">Qui Sommes-Nous</a></li>
+                <li><a href="<?= PROOT ?>/accueil">Accueil</a></li>
+                <li><a href="<?= PROOT ?>/comparatif">Comparatif</a></li>
+                <li><a href="<?= PROOT ?>/qui_sommes_nous">Qui Sommes-Nous</a></li>
             </ul>
-            <div class="copyright">Copyright © 2023. All Rights Reserved.</div>
-        </footer>
-    </body>
+        </nav>
+        <label for="nav-toggle" class="nav-toggle-label">
+            <span></span>
+        </label>
+    </header>
+    <section>
+        <div class="carousel">
+
+            <button class="carousel__button carousel__button--left">
+                <img src="<?= PROOT ?>/img/left.svg" alt="">
+            </button>
+
+            <div class="coursel__track-container">
+                <ul class="carousel__track">
+                    <li class="carousel__slide current-slide">
+                        <img class="carousel__image" src="<?= PROOT ?>/img/img.jpg" alt="">
+                    </li>
+                    <li class="carousel__slide">
+                        <img class="carousel__image" src="<?= PROOT ?>/img/img1.webp" alt="">
+                    </li>
+                    <li class="carousel__slide">
+                        <img class="carousel__image" src="<?= PROOT ?>/img/img2.jpg" alt="">
+                    </li>
+                </ul>
+            </div>
+
+            <button class="carousel__button carousel__button--right">
+                <img src="<?= PROOT ?>/img/right.svg" alt="">
+            </button>
+
+            <div class="carousel__nav">
+                <button class="carousel__indicator current-slide"></button>
+                <button class="carousel__indicator"></button>
+                <button class="carousel__indicator"></button>
+            </div>
+
+        </div>
+    </section>
+    <main class="content-layout">
+        <section></section>
+        <?php
+        $model = new BanqueModel();
+
+        ?>
+        <div class="container">
+            <h1>Select Banks</h1>
+            <form method="post">
+                <div class="select-container">
+                    <div>
+                        <label for="bank1">Select Bank 1:</label>
+                        <select id="bank1" name="bank1">
+                            <?php for ($i = 1; $i <= 20; $i++) { ?>
+                                <option value="<?php echo $i; ?>"><?php echo $model->getBanque($i)->nom; ?></option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                    <div>
+                        <label for="bank2">Select Bank 2:</label>
+                        <select id="bank2" name="bank2">
+                            <?php for ($i = 1; $i <= 20; $i++) { ?>
+                                <option value="<?php echo $i; ?>"><?php echo $model->getBanque($i)->nom; ?></option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                    <input type="submit" value="Submit">
+                </div>
+            </form>
+        </div>
+
+    </main>
+    <footer>
+        <ul>
+            <li><a href="<?= PROOT ?>/accueil">Accueil</a></li>
+            <li><a href="<?= PROOT ?>/comparatif">Comparatif</a></li>
+            <li><a href="<?= PROOT ?>/qui_sommes_nous">Qui Sommes-Nous</a></li>
+        </ul>
+        <div class="copyright">Copyright © 2023. All Rights Reserved.</div>
+    </footer>
+</body>
+
 </html>
 <?php $this->end(); ?>
